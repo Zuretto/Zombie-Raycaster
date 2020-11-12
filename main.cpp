71b0aa4ff8e0ec759e25ceb15d6b3993e8679cd5
mikzur <mikzur4@gmail.com>
2020-11-12 09:57:14 +0100

    copy sfml's hello world, initialize project

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <SFML\Graphics.hpp>
+
+int main() {
+    sf::RenderWindow sfmlWin(sf::VideoMode(600, 360), "Hello World SFML Window");
+    sf::Font font;
+    //You need to pass the font file location
+    if (!font.loadFromFile(/*
+                           Put the filename that identify the font file you want to load*/"Shiny Signature.ttf")) {
+        return -1;
+    }
+    sf::Text message("Hello, World !", font);
+
+    while (sfmlWin.isOpen()) {
+
+        sf::Event e;
+        while (sfmlWin.pollEvent(e)) {
+
+            switch (e.type) {
+            case sf::Event::EventType::Closed:
+                sfmlWin.close();
+                break;
+            }
+        }
+
+        sfmlWin.clear();
+        sfmlWin.draw(message);
+        sfmlWin.display();
+    }
+    return 0;
+}
