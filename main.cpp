c6cc5a6fa902c5873dbad05df324478cc1bb2f44
mikzur <mikzur4@gmail.com>
2020-11-13 09:03:05 +0100

    rewrite main, detects UP and DOWN pressed

--- main.cpp
+++ main.cpp
@@ -1,30 +1,48 @@
-#include <SFML\Graphics.hpp>
+#undef main
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <math.h>
+#include "Player.hpp"
+#include "Map.hpp"
+#define screenWidth 480
+#define screenHeight 480
+
 
 int main() {
-    sf::RenderWindow sfmlWin(sf::VideoMode(600, 360), "Hello World SFML Window");
-    sf::Font font;
-    //You need to pass the font file location
-    if (!font.loadFromFile(/*
-                           Put the filename that identify the font file you want to load*/"Shiny Signature.ttf")) {
-        return -1;
-    }
-    sf::Text message("Hello, World !", font);
-
-    while (sfmlWin.isOpen()) {
-
+    Map *map = new Map("test.map", screenWidth, screenHeight);
+    double posX = 22, posY = 12;  //x and y start position
+    double dirX = -1, dirY = 0; //initial direction vector
+    Player *player = new Player(20, 20, 0.5, 0.5, 0, 0.66, map);
+    sf::RenderWindow window2D(sf::VideoMode(screenWidth, screenHeight), "Hello World SFML Window");
+    
+    sf::Clock clock;
+    clock.restart();
+    while (window2D.isOpen()) {
+        sf::Time deltaT = clock.getElapsedTime();
+        //std::cout << deltaT.asSeconds() << std::endl;
+        clock.restart();
         sf::Event e;
-        while (sfmlWin.pollEvent(e)) {
-
+        while (window2D.pollEvent(e)) {
             switch (e.type) {
             case sf::Event::EventType::Closed:
-                sfmlWin.close();
+                window2D.close();
+                break;
+            default:
                 break;
             }
         }
-
-        sfmlWin.clear();
-        sfmlWin.draw(message);
-        sfmlWin.display();
+        window2D.clear();
+        window2D.draw(*map);
+        //map->draw(window2D, screenWidth, screenHeight);
+        
+        window2D.draw(*player);
+        window2D.display();
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
+            player->forward(deltaT);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
+            player->backward(deltaT);
+        }
     }
     return 0;
 }
