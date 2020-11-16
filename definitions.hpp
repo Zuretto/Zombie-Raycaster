1c7f13d8a27d7b72a1626e2d6f9c4659e1e881ff
mikzur <mikzur4@gmail.com>
2020-11-16 21:58:41 +0100

    add definitions and include it in files

--- definitions.hpp
+++ definitions.hpp
@@ -1,5 +1,8 @@
 #pragma once
+#define M_PI 3.14159265358979323846
 #define casterWidth 1024
 #define casterHeight 720
 #define texWidth 32
 #define texHeight 32
+#define entityWidth 64
+#define entityHeight 64
