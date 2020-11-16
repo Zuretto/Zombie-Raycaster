27fd6857bb0b775006bcd62059021003fd2c0137
mikzur <mikzur4@gmail.com>
2020-11-16 21:59:47 +0100

    include definitions

--- Map.hpp
+++ Map.hpp
@@ -1,10 +1,12 @@
 #pragma once
+#include "definitions.hpp"
 #include <SFML/Graphics.hpp>
 #include <string>
 #include <sstream>
 #include <iostream>
 #include <fstream>
 #include <vector>
+
 class Map{
 private:
     std::vector <std::vector <int>> map_vector;
@@ -14,5 +16,4 @@ public:
     std::vector <std::vector <int>> getMapVector() const;
     Map(std::string fileName);
     ~Map();
-    //void draw(sf::RenderTarget &target, int width, int height);
 };
