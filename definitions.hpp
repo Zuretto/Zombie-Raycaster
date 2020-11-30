e43a68ffd2f41e35331f21f6731564d8c1a6610a
mikzur <mikzur4@gmail.com>
2020-11-30 01:39:34 +0100

    definitions for weapons and enemies

--- definitions.hpp
+++ definitions.hpp
@@ -1,8 +1,13 @@
 #pragma once
 #define M_PI 3.14159265358979323846
+#define weapon_states 2
 #define casterWidth 1024
 #define casterHeight 720
 #define texWidth 32
 #define texHeight 32
 #define entityWidth 64
 #define entityHeight 64
+#define enemyWidth 64
+#define enemyHeight 64
+#define weaponWidth 128
+#define weaponHeight 128
