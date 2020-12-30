13ed640424c8dd7ff22afb2d15d26013cd0f2809
mikzur <mikzur4@gmail.com>
2020-12-30 23:34:07 +0100

    add performShoot method

--- Weapon.cpp
+++ Weapon.cpp
@@ -1,6 +1,8 @@
 #include "Weapon.hpp"
 Weapon::Weapon(){}
-Weapon::~Weapon(){}
+Weapon::~Weapon(){
+    std::cout << "Deleted Weapon" << std::endl;
+}
 int Weapon::getType(){
     return WEAPON;
 }
@@ -10,6 +12,29 @@ int Weapon::getDamage(){
 int Weapon::getRange(){
     return range;
 }
+void Weapon::performShoot(Player *player, Map *worldMap, std::vector <std::shared_ptr<Enemy>> &enemies){
+    double step = 0.1; 
+    double dirX = player->getdirX();
+    double dirY = player->getdirY();
+    double posX = player->getPosX();
+    double posY = player->getPosY();
+    std::vector <std::vector <int>> mapVect = worldMap->getMapVector();
+    while(mapVect[posX][posY] == 0){
+        posX += dirX * step;
+        posY += dirY * step;
+        bool hitEnemy = 0;
+        for(auto enemy : enemies){
+            if(sqrt(pow(enemy->getPosX() - posX, 2) + pow(enemy->getPosY() - posY, 2)) < enemy->getRadius() && enemy->getHp() > 0){
+                enemy->lowerHp(damage);
+                std::cout << enemy->getHp() << std::endl;
+                hitEnemy = 1;
+                break;
+            }
+        }
+        if (hitEnemy) break;
+    }
+}
+
 Pistol::Pistol(){
     damage = 40;
     range = 20;
--- Weapon.hpp
+++ Weapon.hpp
@@ -1,19 +1,27 @@
 #pragma once
 #include "definitions.hpp"
 #include "SFML/Graphics.hpp"
-enum gun_enum {WEAPON, FIST, PISTOL, SHOTGUN, GUN_TYPES_NUMBER};
+#include "Map.hpp"
+#include "Enemy.hpp"
+#include <memory>
+
+class Player;
+class Enemy;
 class Weapon{
 protected:
     int damage;
     int range;
     int ammo;
     double shotCooldown;
+    sf::Clock gunClock;
+
 public:
     Weapon();
     virtual ~Weapon();
     virtual int getType();
     int getDamage();
     int getRange();
+    virtual void performShoot(Player *player, Map *worldMap, std::vector <std::shared_ptr<Enemy>> &enemies);
 };
 
 class Pistol : public Weapon{
@@ -22,4 +30,6 @@ public:
     ~Pistol();
     int getType() override;
 };
+
+
 
