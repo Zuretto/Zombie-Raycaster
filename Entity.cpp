15a6830e8f0548fa6ef3f343e883e4c0919d4c0d
mikzur <mikzur4@gmail.com>
2021-01-01 21:50:54 +0100

    add couldDelete,onApproach actions, implement ammo

--- Entity.cpp
+++ Entity.cpp
@@ -16,24 +16,97 @@ double Entity::getPosY() const {
 double Entity::getRadius(){
     return radius;
 }
-
+void Entity::onApproach(Player *player){}
+bool Entity::canBeDeleted(){
+    return couldDelete;
+}
 Barrel::Barrel(double xPos, double yPos){
     posX = xPos;
     posY = yPos;
     radius = 0.5;
+    xVelocity = 0;
+    yVelocity = 0;
+    couldDelete = 0;
 }
 Barrel::~Barrel(){
 }
 int Barrel::getType(){
     return BARREL;
 }
+
+void Barrel::onApproach(Player *player){
+    double distance = sqrt(pow(this->getPosX() - player->getPosX(), 2) + pow(this->getPosY() - player->getPosY(), 2));
+    if(distance < player->getRadius() + this->getRadius()){
+        xVelocity = player->getMoveSpeed() * player->getdirX();
+        yVelocity = player->getMoveSpeed() * player->getdirY();
+    }
+}
+
+void Barrel::onUpdate(sf::Time deltaT, Map *map){
+    auto map_vect = map->getMapVector();
+    
+    double xRadius = -radius * ((xVelocity < 0) - (0 < xVelocity));
+    double yRadius = -radius * ((yVelocity < 0) - (0 < yVelocity));
+    double futureX = posX + xRadius + xVelocity * deltaT.asSeconds();
+    double futureY = posY + yRadius + yVelocity * deltaT.asSeconds();
+
+    if(map_vect[int(futureX)][int(posY + yRadius)] > 0) xVelocity = -xVelocity;
+    if(map_vect[int(posX + xRadius)][int(futureY)] > 0) yVelocity = -yVelocity;
+    this->posX += xVelocity * deltaT.asSeconds();
+    this->posY += yVelocity * deltaT.asSeconds();
+    if(xVelocity > 0){
+       xVelocity -= movementDelay * deltaT.asSeconds();
+    } else if (xVelocity < 0) xVelocity += movementDelay * deltaT.asSeconds();
+    if(yVelocity > 0){
+       yVelocity -= movementDelay * deltaT.asSeconds();
+    } else if (yVelocity < 0) yVelocity += movementDelay * deltaT.asSeconds();
+}
+
 Pillar::Pillar(double xPos, double yPos){
     posX = xPos;
     posY = yPos;
     radius = 0.5;
+    couldDelete = 0;
 }
 Pillar::~Pillar(){
 }
 int Pillar::getType(){
     return PILLAR;
+}
+void Pillar::onApproach(Player *player){}
+
+Ammo_Shotgun::Ammo_Shotgun(double xPos, double yPos){
+    posX = xPos;
+    posY = yPos;
+    radius = 0.5;
+    couldDelete = 0;
+}
+Ammo_Shotgun::~Ammo_Shotgun(){}
+int Ammo_Shotgun::getType(){
+    return AMMO_SHOTGUN;
+}
+void Ammo_Shotgun::onApproach(Player *player){
+    double distance = sqrt(pow(this->getPosX() - player->getPosX(), 2) + pow(this->getPosY() - player->getPosY(), 2));
+    if(distance < player->getRadius() + this->getRadius()){
+        player->increaseAmmo(SHOTGUN, ammoStackAmount);
+        this->couldDelete = 1;
+    }
+}
+
+Ammo_Pistol::Ammo_Pistol(double xPos, double yPos){
+    posX = xPos;
+    posY = yPos;
+    radius = 0.5;
+    couldDelete = 0;
+}
+Ammo_Pistol::~Ammo_Pistol(){}
+int Ammo_Pistol::getType(){
+    return AMMO_PISTOL;
+}
+void Ammo_Pistol::onApproach(Player *player){
+    double distance = sqrt(pow(this->getPosX() - player->getPosX(), 2) + pow(this->getPosY() - player->getPosY(), 2));
+    if(distance < player->getRadius() + this->getRadius()){
+        player->increaseAmmo(PISTOL, ammoStackAmount);
+        this->couldDelete = 1;
+    }
 }
--- Entity.hpp
+++ Entity.hpp
@@ -1,27 +1,39 @@
 #pragma once
 #include "SFML/Graphics.hpp"
 #include "definitions.hpp"
+#include "Player.hpp"
 
-
+class Player;
 class Entity{
 protected:
     double posX;
     double posY;
     double radius;
+    bool   couldDelete; // 0 if Game shouldn't delete the entity, 1 if should
 public:
     Entity();
     virtual ~Entity();
     virtual int getType();
     double getPosX() const;
     double getPosY() const;
+    bool   canBeDeleted();
     double getRadius();
+    virtual void onApproach(Player *player); //checks if Player is close enough and then does sth with that information
+    virtual void onUpdate(sf::Time deltaT, Map *map){}                 //performs onUpdate actions like moving in some direction (or nothing)
+    //virtual void onUpdateEntity();
 };
 
 class Barrel : public Entity{
+protected:
+    static constexpr double movementDelay = 4; //meters per square seconds
+    double xVelocity;
+    double yVelocity;
 public:
     Barrel(double posX, double posY);
     ~Barrel();
     int getType() override;
+    void onApproach(Player *player) override;
+    void onUpdate(sf::Time deltaT, Map *map) override;
 };
 
 class Pillar : public Entity{
@@ -29,4 +41,25 @@ public:
     Pillar(double posX, double posY);
     ~Pillar();
     int getType() override;
+    void onApproach(Player *player) override;
+};
+
+class Ammo_Shotgun : public Entity{
+private:
+    static constexpr int ammoStackAmount = 8;
+public:
+    Ammo_Shotgun(double posX, double posY);
+    ~Ammo_Shotgun();
+    int getType() override;
+    void onApproach(Player *player) override;
+};
+
+class Ammo_Pistol : public Entity{
+private:
+    static constexpr int ammoStackAmount = 12;
+public:
+    Ammo_Pistol(double posX, double posY);
+    ~Ammo_Pistol();
+    int getType() override;
+    void onApproach(Player *player) override;
 };
