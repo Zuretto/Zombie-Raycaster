7e890407ea247da24a4c86a51373c1ba8e154c7b
mikzur <mikzur4@gmail.com>
2021-01-01 21:48:40 +0100

    put weapon_states number into enum

--- definitions.hpp
+++ definitions.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #define M_PI 3.14159265358979323846
-#define weapon_states 2
 #define casterWidth 1024
 #define casterHeight 720
 #define texWidth 32
@@ -12,12 +11,16 @@
 #define weaponWidth 128
 #define weaponHeight 128
 
-enum gun_enum {WEAPON, FIST, PISTOL, SHOTGUN, GUN_TYPES_NUMBER};
 
-enum gun_states {DRAWN, SHOOT, WEAPON_STATES_NUMBER};
+enum weapon_enum {WEAPON, FIST, PISTOL, SHOTGUN, WEAPON_TYPES_NUMBER};
+
+enum weapon_states {DRAWN, SHOT1, SHOT2, SHOT3, WEAPON_STATES_NUMBER};
 
 enum enemy_states {ALIVE, DIED1, DIED2, DEAD, ENEMY_STATES_NUMBER};
 
 enum enemy_num {ENEMY, ZOMBIE, SKELETON, ENEMY_TYPES_NUMBER};
 
-enum entity_enum {ENTITY, BARREL, TABLE, LAMP, PILLAR, ENTITY_TYPES_NUMBER};
+enum entity_enum {ENTITY, BARREL, PILLAR, AMMO_PISTOL, AMMO_SHOTGUN, ENTITY_TYPES_NUMBER};
+
+
+
